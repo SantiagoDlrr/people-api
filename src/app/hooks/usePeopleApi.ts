eb77a4ce8ea67dcaf86ac4dc512c56e7718351c5
SantiagoDlrr <A01749140@tec.mx>
2025-03-24 11:32:08 -0600

    Structure

---
+++ src/app/hooks/usePeopleApi.ts
@@ -0,0 +1,19 @@
+// usePeopleApi hook
+/*
+- Variables, person, person history, error, loading 
+- Fetch data with axios 
+- save data and then create a Person
+- Set the person and save it to the history 
+- Catch error and finally loading to true. 
+- Use effect to call the fetched data 
+- Return the variables and const fetch data
+
+*/
+
+'use client'
+import { useState, useEffect} from "react"
+
+export default function usePeopleApi () {
+
+
+}
---
+++ src/app/types/person.ts
@@ -0,0 +1,10 @@
+/*interface of person with
+name em birth phone and pass */
+
+export interface Person {
+    name: string
+    email: string
+    birthday: string
+    phone: string
+    password: string
+}
