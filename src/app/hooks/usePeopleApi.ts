d3043bc798bcb1c6dc864c82777ab2027f994ca5
SantiagoDlrr <A01749140@tec.mx>
2025-03-24 11:32:24 -0600

    Structure

--- src/app/hooks/usePeopleApi.ts
+++ src/app/hooks/usePeopleApi.ts
@@ -1,6 +1,7 @@
 // usePeopleApi hook
 /*
 - Variables, person, person history, error, loading 
+
 - Fetch data with axios 
 - save data and then create a Person
 - Set the person and save it to the history 
@@ -11,9 +12,17 @@
 */
 
 'use client'
-import { useState, useEffect} from "react"
+import { useState, useEffect} from "react";
+import axios from 'axios';
+import {Person} from "../types/person";
 
 export default function usePeopleApi () {
+    const [person, setPerson] = useState<Person | null>(null);
+    const [personHistory, setHistory] = useState<Person[] | null>(null);
+    const [error, setError] = useState<boolean>(false);
+    const [loading, setLoading] = useState<boolean>(false);
 
-
+    const fetchData = async () => {
+        const response = await axios.get('https://randomuser.me/api/')
+    }
 }
